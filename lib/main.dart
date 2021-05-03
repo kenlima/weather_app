d52191286f5e4f2160369fb3bc3f210937f5a7b5
jwlee <jwlee@jwleeui-MacBook-Pro.local>
2021-05-03 16:41:46 +0900

    완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:weather_app/models/src/app_settings.dart';
+import 'package:weather_app/page/page_container.dart';
+import 'package:weather_app/styles.dart';
 
 void main() {
-  runApp(MyApp());
+  AppSettings settings = AppSettings();
+
+  // Don't allow landscape mode
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(MyApp(settings: settings)));
 }
 
 class MyApp extends StatelessWidget {
+  final AppSettings settings;
+
+  const MyApp({Key key, this.settings}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    final theme = ThemeData(
+      fontFamily: "Cabin",
+      primaryColor: AppColor.midnightSky,
+      accentColor: AppColor.midnightCloud,
+      primaryTextTheme: Theme.of(context).textTheme.apply(
+            bodyColor: AppColor.textColorDark,
+            displayColor: AppColor.textColorDark,
+          ),
+      textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: AppColor.textColorDark,
+            displayColor: AppColor.textColorDark,
+          ),
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: PageContainer(settings: settings),
     );
   }
 }
